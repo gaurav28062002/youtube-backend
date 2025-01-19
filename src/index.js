39c499743c0acd898f1857059ec96c4358fc003e
Gaurav Gajbhare <98952569+gaurav28062002@users.noreply.github.com>
2025-01-19 17:11:41 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ dotenv.config({
 });
 
 import connectDB from "./db/index.js";
+import app from "./app.js";
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+    
+});
 
 // (async () => {
 //   try {
