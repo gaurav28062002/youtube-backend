1a297c5715d2650c658f45ae2aabf22938a03b18
Gaurav Gajbhare <98952569+gaurav28062002@users.noreply.github.com>
2025-01-20 19:11:14 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,4 +12,9 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//imoport routes
+import userRoutes from './routes/user.routes.js';
+//routes declaration
+app.use('/api/v1/users', userRoutes);
 export default app;
